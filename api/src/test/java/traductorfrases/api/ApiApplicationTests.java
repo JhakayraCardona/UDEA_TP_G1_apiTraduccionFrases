390d258292ef7fb0a0dfd9171f976bcc30de52b2
unknown <frayosorio@gmail.com>
2026-05-11 20:35:04 -0500

    Se incluyen scripts para crear y poblar Bd TRADUCCCION DE FRASES y se crea el proyecto de la API respectiva

---
+++ api/src/test/java/traductorfrases/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package traductorfrases.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
